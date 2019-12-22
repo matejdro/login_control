5459dce74c866a7644c41ce3bff5b6f41a4b60f3
Matej Drobnič <matejdro@gmail.com>
2019-12-22 09:15:53 +0100

    Initial commit

---
+++ login_control/__init__.py
@@ -0,0 +1,19 @@
+import logging
+
+DOMAIN = 'token_control'
+
+async def async_setup(hass, config):
+
+    async def handle_refresh_token_clear(call):
+        user_id = call.data.get("user_id")
+        user = await hass.auth.async_get_user(user_id)
+
+        tokens = list(user.refresh_tokens.values())
+
+        for token in tokens:
+            await hass.auth.async_remove_refresh_token(token)
+
+    hass.services.async_register(DOMAIN, 'clear_refresh_tokens', handle_refresh_token_clear)
+
+    # Return boolean to indicate that initialization was successful.
+    return True
